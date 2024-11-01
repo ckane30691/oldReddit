8ff4e5c7e3de1ee0ef8901ed47fc69080528b8ec
Cory Kane <ckane30691@gmail.com>
2024-11-01 13:02:13 -0700

    Move over get all subreddits endpoint to dynamodb

--- routes/api/subs/readAll.js
+++ routes/api/subs/readAll.js
@@ -1,28 +1,23 @@
-const mongoose = require('mongoose');
-const keys = require('../../../config/keys');
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
 const SubReddit = require('../../../models/SubReddit');
 
-
+// TODO: Add caching to this function
 exports.handler = async (event) => {
-    try {
-        const queryParams = event.queryStringParameters;
-        const userId = queryParams?.filters;
+	try {
+		// const queryParams = event.queryStringParameters;
+		// const userId = queryParams?.filters;
 
-        let subReddits = !userId
-            ? await SubReddit.find()
-            : await SubReddit.find({ userId });
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(subReddits)
-        };
-    } catch (err) {
-
-        return {
-            statusCode: 404,
-            body: JSON.stringify({ noSubRedditsFound: 'No subReddits found' })
-        }
-    }
-}
+		// let subReddits = !userId
+		// 	? await SubReddit.find()
+		// 	: await SubReddit.find({ userId });
+		const subReddits = await SubReddit.scan().exec();
+		return {
+			statusCode: 200,
+			body: JSON.stringify(subReddits),
+		};
+	} catch (err) {
+		return {
+			statusCode: 404,
+			body: JSON.stringify({ noSubRedditsFound: 'No subReddits found' }),
+		};
+	}
+};
