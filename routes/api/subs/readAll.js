072b5e9d5db0b316923edea040b535929d452d71
Cory Kane <ckane30691@gmail.com>
2024-11-08 19:22:35 -0700

    Add redis caching to subreddit endpoint

--- routes/api/subs/readAll.js
+++ routes/api/subs/readAll.js
@@ -1,6 +1,11 @@
+const redisClient = require('../../../config/redisClient');
 const SubReddit = require('../../../models/SubReddit');
+const { easyParse } = require('../../../utils/pagination');
 
-// TODO: Add caching to this function
+(async () => {
+	await redisClient.connect().catch(console.error);
+})();
+
 exports.handler = async (event) => {
 	try {
 		// const queryParams = event.queryStringParameters;
@@ -9,7 +14,31 @@ exports.handler = async (event) => {
 		// let subReddits = !userId
 		// 	? await SubReddit.find()
 		// 	: await SubReddit.find({ userId });
+
+		const cacheKey = `subReddits:`; // Create a unique cache key
+
+		// Check Redis cache first
+		const cachedSubReddits = await redisClient.get(cacheKey);
+
+		if (cachedSubReddits) {
+			console.log('Cache hit for subReddits');
+			const { subReddits } = easyParse(cachedSubReddits);
+
+			return {
+				statusCode: 200,
+				body: JSON.stringify(subReddits),
+			};
+		}
+
 		const subReddits = await SubReddit.scan().exec();
+
+		//Cache the results with an expiration time
+		await redisClient.set(
+			cacheKey,
+			JSON.stringify({ subReddits }),
+			'EX',
+			60 * 60
+		); // Cache for 1 hour
 		return {
 			statusCode: 200,
 			body: JSON.stringify(subReddits),
