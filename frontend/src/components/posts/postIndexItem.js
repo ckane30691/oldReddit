c4b376586d6c87aadab85907253a722b76396d38
Cory Kane <ckane30691@gmail.com>
2025-01-08 17:09:37 -0700

    Remove more unused variables

--- frontend/src/components/posts/postIndexItem.js
+++ frontend/src/components/posts/postIndexItem.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { deletePost } from '../../store/slices/entities/postSlice';
-require('./postIndexItem.css')
+// import { deletePost } from '../../store/slices/entities/postSlice';
+require('./postIndexItem.css');
 
 export const PostIndexItem = ({ post }) => {
-	const dispatch = useDispatch();
-	const handleDelete = async (e) => {
-		// let res;
-		e.preventDefault();
-		/*res =*/ await dispatch(deletePost(post._id));
-	};
+	// const dispatch = useDispatch();
+	// const handleDelete = async (e) => {
+	// 	// let res;
+	// 	e.preventDefault();
+	// 	/*res =*/ await dispatch(deletePost(post._id));
+	// };
 	return (
-		<li className='link-container'>
+		<li className="link-container">
 			<h1>
-				<Link className='post-link' to={`/posts/${post._id}`}>{post.title}</Link>
+				<Link className="post-link" to={`/posts/${post._id}`}>
+					{post.title}
+				</Link>
 				{/* <button onClick={handleDelete}>Delete</button> */}
 			</h1>
 		</li>
