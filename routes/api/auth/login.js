906b90108bac4475bb9e595633923bf1880d841f
Cory Kane <ckane30691@gmail.com>
2025-11-19 19:30:24 -0700

    Fix typo in login and change NODE env to prod

--- routes/api/auth/login.js
+++ routes/api/auth/login.js
@@ -64,9 +64,9 @@ exports.handler = async (event) => {
 
 		return {
 			statusCode: 200,
+			headers,
 			body: JSON.stringify({
 				success: true,
-				headers,
 				token: 'Bearer ' + token,
 			}),
 		};
